69206a3493ca1afa7c9bcaf4033d6de2b0c7428a
1227804281 <cjorda33@asu.edu>
2025-12-10 13:45:36 -0800

    updates

--- script.js
+++ script.js
@@ -2,17 +2,4 @@
  let largeButton = document.getElementsByClassName("BigBoy");
  let smallButton = document.getElementsByClassName("SmallBoy");
 
- let i;
-
- for (i = 0; i < largeButton.length; i++) {
-    largeButton[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-
-        let panel = this.nextElementSiblig;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
- }
+//  i dont have time for javascript actually ahh
